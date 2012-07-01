a813c9ab1218d3e0a56f7022ccb39dab57c0e66f
gwright <gwright@antiope.com>
2012-07-01 15:31:49 -0400

    Add FactorOut.hs
    Ignore-this: 276ca038e6704933b468f396fb1bb173
    
    darcs-hash:20120701193149-870a6-879ec0c63f70ad6c2a2d79f116518c79a6b781fa.gz

---
+++ src/Math/Symbolic/Wheeler/FactorOut.hs
@@ -0,0 +1,58 @@
+--
+-- FactorOut.hs
+--
+-- Factor something out of an expression.
+--
+-- Gregory Wright, 6 June 2012
+--
+
+module Math.Symbolic.Wheeler.FactorOut where
+
+
+import Data.Maybe
+
+import Math.Symbolic.Wheeler.Canonicalize
+import Math.Symbolic.Wheeler.Expr
+import Math.Symbolic.Wheeler.MetricTensor
+import Math.Symbolic.Wheeler.Symbol
+import Math.Symbolic.Wheeler.Tensor
+import Math.Symbolic.Wheeler.TensorUtilities
+
+
+
+mapExpr :: (Expr -> Expr) -> Expr -> Expr
+mapExpr f (Sum ts)     = f (Sum (map f ts))
+mapExpr f (Product fs) = f (Product (map f fs))
+mapExpr f e = f e
+
+
+containsTensor :: Expr -> Expr -> Bool
+containsTensor t e = isJust $ findExpr (equalUpToVariance t) e
+
+
+factorOutTensor :: Expr -> Expr -> Expr
+factorOutTensor t e@(Sum ts) =
+  let
+    allTermsHaveTensor = all (containsTensor t) ts
+    t' = dummyize t
+  in
+   if allTermsHaveTensor
+      then canonicalize $ t' * (Sum (map (replaceWithKroneckers t t') ts))
+      else e
+    
+factorOutTensor _ e = e
+
+
+replaceWithKroneckers :: Expr -> Expr -> Expr -> Expr
+replaceWithKroneckers t@(Symbol (Tensor tt)) t' e =
+  let
+    (t'', bc)   = fromJust $ findExpr' (equalUpToVariance t) e
+    m           = manifold tt
+    d           = \ x y -> if sameVariance x y
+                           then mkKroneckerDelta_ m "delta" "\\delta" (-x) y
+                           else mkNamedMetric m "g" "g" (-x) y
+    replacement = Product $ zipWith d (tensorIndices t') (tensorIndices t'')
+  in
+    replaceExpr bc replacement e
+    
+replaceWithKroneckers _ _ e = e    
